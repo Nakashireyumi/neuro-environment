ad630b29e3da8d93c397bae5e8c6338c04c3295d
Cassitly <102461124+cassitly@users.noreply.github.com>
2025-11-10 09:04:08 +0700

    Implement CVM and CVMConfig classes
    Added CVM and CVMConfig classes for Cassitly VM.

---
+++ src/dev/cassitly/cvm.py
@@ -0,0 +1,9 @@
+class CVMConfig
+
+class CVM:
+  """
+  Cassitly VM (cvm) is an binary loader that allows cross operation between different languages and python
+  """
+
+  def __init__(self, config:):
+    
